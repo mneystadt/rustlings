1b03982a5295336ae8445dd25ead07d6df506b01
Mark Neystadt <neystadt.mark@gmail.com>
2024-04-18 13:08:02 +0200

    conversions passed

--- exercises/conversions/from_into.rs
+++ exercises/conversions/from_into.rs
@@ -40,10 +40,27 @@ impl Default for Person {
 // If while parsing the age, something goes wrong, then return the default of
 // Person Otherwise, then return an instantiated Person object with the results
 
-// I AM NOT DONE
 
 impl From<&str> for Person {
     fn from(s: &str) -> Person {
+        match s.split_once(',') {
+            Some((name, age)) => {
+                if name.is_empty() {
+                    Person::default()
+                } else if let Ok(a) = age
+                    .split(',')
+                    .collect::<Vec<_>>()[0] // ugly but gets the job done
+                    .parse::<usize>() {
+                    Person {
+                        name: name.into(),
+                        age: a,
+                    }
+                } else {
+                    Person::default()
+                }
+            },
+            _ => Person::default(),
+        }
     }
 }
 
